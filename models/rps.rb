4b6dca058d4c0e149734aac4b28219a64c48e921
Ricky <rickycorrigan@icloud.com>
2018-08-06 21:04:09 +0100

    if statement added to class

--- models/rps.rb
+++ models/rps.rb
@@ -6,6 +6,20 @@ class RPS
   end
 
   def result()
-
+    if @player_1 == "rock" && @player_2 == "scissors"
+      return "Rock Wins"
+    elsif @player_1 == "rock" && @player_2 == "paper"
+      return "Paper Wins"
+    elsif @player_1 == "paper" && @player_2 == "scissors"
+      return "Scissors Wins"
+    elsif @player_1 == "paper" && @player_2 == "rock"
+      return "Paper Wins"
+    elsif @player_1 == "scissors" && @player_2 == "rock"
+      return "Rock Wins"
+    elsif @player_1 == "scissors" && @player_2 == "paper"
+      return "Scissors Wins"
+    elsif @player_1 == @player_2
+      return "Play Again"
+    end
   end
 end
